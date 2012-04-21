8044117eeb60d422e1767a61520a2cc4123aad4c
David Peter <david.a.peter@gmail.com>
2012-04-20 19:58:24 -0700

    Add name entry and beginning of scene 1.

--- javascripts/main.coffee
+++ javascripts/main.coffee
@@ -1,13 +1,33 @@
 $message = $("#message")
 
-@message = (msg, cont = false) ->
+show = (id) ->
+  $("##{id}").removeClass('hidden')
+
+message = (msg, cont = false) ->
+  $message.removeClass('hidden')
   $message.html(msg)
   if cont then $message.append("""
     <div id="continue">
       #{cont} <img src="./images/buttons/continue.gif" alt="continue"/>
     </div>""")
 
-
 # Game start
 $ ->
-  message "hello world!", "GG is typing..."
+  $nameInsert = $('#nameinsert')
+  $nameInsert.show()
+  $('#newname').focus()
+  $nameInsert.find('input').on 'keyup', (e) ->
+    if e.which == 13 then enteredName()
+  $nameInsert.on 'click', '.ok', ->
+    enteredName()
+
+  enteredName = ->
+    newName = $('#newname').val()
+    if newName == "Googlohurf Harpstuck"
+      alert "Oh, come on! That's a horrible name."
+      $('#newname').focus()
+      return
+    $nameInsert.find('.first').remove()
+    $nameInsert.find('.second').show().on 'click', '.ok', ->
+      $nameInsert.remove()
+      show 'scene1'
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,15 @@
 (function() {
-  var $message;
+  var $message, message, show;
 
   $message = $("#message");
 
-  this.message = function(msg, cont) {
+  show = function(id) {
+    return $("#" + id).removeClass('hidden');
+  };
+
+  message = function(msg, cont) {
     if (cont == null) cont = false;
+    $message.removeClass('hidden');
     $message.html(msg);
     if (cont) {
       return $message.append("<div id=\"continue\">\n  " + cont + " <img src=\"./images/buttons/continue.gif\" alt=\"continue\"/>\n</div>");
@@ -12,7 +17,30 @@
   };
 
   $(function() {
-    return message("hello world!", "GG is typing...");
+    var $nameInsert, enteredName;
+    $nameInsert = $('#nameinsert');
+    $nameInsert.show();
+    $('#newname').focus();
+    $nameInsert.find('input').on('keyup', function(e) {
+      if (e.which === 13) return enteredName();
+    });
+    $nameInsert.on('click', '.ok', function() {
+      return enteredName();
+    });
+    return enteredName = function() {
+      var newName;
+      newName = $('#newname').val();
+      if (newName === "Googlohurf Harpstuck") {
+        alert("Oh, come on! That's a horrible name.");
+        $('#newname').focus();
+        return;
+      }
+      $nameInsert.find('.first').remove();
+      return $nameInsert.find('.second').show().on('click', '.ok', function() {
+        $nameInsert.remove();
+        return show('scene1');
+      });
+    };
   });
 
 }).call(this);
