3701e4b4ebe01cca3a1f35fff45fbd73be3d69b3
David Peter <david.a.peter@gmail.com>
2012-04-21 10:41:59 -0700

    Add gfx + functionality for room 1.

--- javascripts/main.coffee
+++ javascripts/main.coffee
@@ -1,5 +1,9 @@
 $document = $(document)
 $message = $("#message")
+$content = $("#content")
+
+state =
+  yourDoorLocked: true
 
 show = (id) ->
   find(id).removeClass('hidden')
@@ -27,11 +31,21 @@ message = (msg, cont = "") ->
     <img src="./images/buttons/continue.gif" alt="continue"/>
   </div>""")
   $message.scrollTop(0)
+  show "messagehitbox"
   message.up = true
+
+control = ($this) ->
+  control.person = $this
+control.target = {}
+
+debug = (msg...) ->
+  find('debug').html(msg.join('<br>'))  if debug.on
+debug.on = true
 
 hideMessage = ->
   if message.up
     $("#message").addClass('hidden')
+    find('messagehitbox').addClass('hidden')
     message.up = false
     $document.trigger 'messageend'
 hideMessage()
@@ -40,10 +54,19 @@ $(document).keyup (e) ->
   if message.up and e.which == 13 || e.which == 90
     hideMessage()
 
-# Game start
-$ ->
-  $nameInsert = $('#nameinsert')
-  $nameInsert.show()
+$document.on 'click', '#messagehitbox', ->
+  hideMessage()
+
+# Mouse movements
+$content.on 'mousedown', (e) ->
+  if control.person?
+    x = e.pageX - @offsetParent.offsetLeft
+    y = e.pageY - @offsetParent.offsetTop
+    control.target = {x, y}
+    debug x, y
+
+enterName = ->
+  $nameInsert = show 'nameinsert'
   $('#newname').focus()
   $nameInsert.find('input').on 'keyup', (e) ->
     if e.which == 13 then enteredName()
@@ -52,23 +75,43 @@ $ ->
 
   enteredName = ->
     newName = $('#newname').val()
-    if newName == "Googlohurf Harpstuck"
+    if newName == "Googlohurf Hoodchuck"
       alert "Oh, come on! That's a horrible name."
       $('#newname').focus()
       return
     $nameInsert.find('.first').remove()
     $nameInsert.find('.second').show().on 'click', '.ok', ->
       $nameInsert.remove()
-      show 'scene1'
-      setTimeout scene1, 0
-      # setTimeout scene1, 3000
+      introYourRoom()
 
+play1 = ->
+  $play = show 'play1'
+  message "This is you! You're a pretty handsome dude :)"
+  $document.on 'messageend', ->
+    enterName()
+
+introYourRoom = ->
+  $scene = show 'scene1'
+  # TODO: Make room
+  message """
+  This is your room. It's a bit dark right now, even if it means getting around is a little harder.
+  """
+  $document.one 'messageend', ->
+    $scene.find('.yourbroim').removeClass('hidden')
+    message """
+    Looks like your bro is instant messaging you! You have an inkling of what he's got to say, and you don't want to miss it.
+    """
+    $document.one 'messageend', ->
+      $scene.find('.yourbroim').on 'click', ->
+        $scene.find('.yourbroim').remove()
+        scene1()
 
 # Scene 1
 scene1 = ->
-  $scene = find 'scene1'
+  $scene = show 'scene1'
+  $scene.find('.ggcomp').removeClass('hidden')
   message """
-  * LL began instant-messaging you!
+  * LL began instant messaging you!
   LL: Hello, Steve.
   LL: I just had another revelation.
   GQ: hahaha dude
@@ -83,7 +126,7 @@ scene1 = ->
     message """
     LL: I think we're in a game.
     GQ: O_O
-    LL: Let me talk.
+    LL: No, let me talk.
     LL: So, I've been wondering.
     LL: Our lives really aren't that different from a video game character.
     LL: We level up, we have arbitrary stat attributes, and all that.
@@ -115,13 +158,77 @@ scene1 = ->
             GQ: uh
             GQ: pretty melodramatic there bro!!
             LL: Okay, well, I guess it's not surprising that you'd act this way.
-            LL: But, speaking as your friend, I know you're freaked out.
-            LL: My work here is done.
+            LL: But, speaking as your younger brother by blood, you should really quit it.
+            LL: And this isn't one of those times where the very next day I realize you were absolutely correct.
+            LL: Anyway, I have to go fix my dimensional warper. It should be done very soon.
+            LL: Be in my room in two minutes.
+            * LL signed off.
+            GQ: wait what
+            * LL is no longer online!
             """
+            $document.one 'messageend', play2
+
+play2 = ->
+  $scene = show 'scene1'
+  $scene.find('.ggcomp').remove()
+  $scene.find('.gghouse').remove()
+  $scene.find('.stars').remove()
+  $scene.find('.tentacles').remove()
+  message """
+  Getting around this house is impossible! You know it'll take you way longer than two minutes. But you have to try. Your brother might be annoying slash amusing slash cute with his "revelations," but he always makes cool stuff.
+  """
+  $document.one 'messageend', ->
+    message """
+    Your mother is extremely private, so she installed switches everywhere to deter anyone from stealing her things (or her kids). She is especially protective of your little brother and never lets him unlock his own door.
+
+    You can't remember the exact sequences to the switches, because she changes them every night. Your mother somehow manages to change them without you noticing. And sometimes, some switches depend on another switch being pressed.
+
+    You have no idea how she keeps track of all these switches in her head. It would be impressive if it wasn't SO INFURIATING.
+    """
+    $document.one 'messageend', ->
+      $scene.find('.doorswitch').removeClass('hidden')
+      $scene.find('.lightswitch').removeClass('hidden')
+      $scene.find('.door').removeClass('hidden')
+      $scene.find('.door').on 'click', ->
+        if state.yourDoorLocked
+          message """
+          The door is locked!
+          """
+        else
+          scene2()
+      $scene.find('.doorswitch').on 'click', ->
+        if state.flippedLight
+          message """
+          You hear a *click*.
+          """
+          state.yourDoorLocked = false
+        else
+          message """
+          The switch won't budge! It must depend on another switch...
+          """
+      $scene.find('.lightswitch').on 'click', ->
+        if state.flippedLight
+          message """
+          You turn off the lights.
+
+          You also hear a *click*.
+          """
+          state.flippedLight = false
+        else
+          message """
+          You turn on the lights. You also resist a light pun.
+
+          You also hear a *click*.
+          """
+          state.flippedLight = true
+
+
 
 # Scene 2
 scene2 = ->
+  message """
+  Navigate!
+  """
 
-
-$ ->
-  scene1()
+# Game start
+$ play2
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,16 @@
 (function() {
-  var $document, $message, colorize, find, hideMessage, message, scene1, scene2, show;
+  var $content, $document, $message, colorize, control, debug, enterName, find, hideMessage, introYourRoom, message, play1, play2, scene1, scene2, show, state,
+    __slice = Array.prototype.slice;
 
   $document = $(document);
 
   $message = $("#message");
+
+  $content = $("#content");
+
+  state = {
+    yourDoorLocked: true
+  };
 
   show = function(id) {
     return find(id).removeClass('hidden');
@@ -32,12 +39,28 @@
     $message.empty().html(msg.replace(/\n/g, '<br>'));
     $message.append("<div id=\"continue\">\n  " + cont + "\n  <img src=\"./images/buttons/continue.gif\" alt=\"continue\"/>\n</div>");
     $message.scrollTop(0);
+    show("messagehitbox");
     return message.up = true;
   };
+
+  control = function($this) {
+    return control.person = $this;
+  };
+
+  control.target = {};
+
+  debug = function() {
+    var msg;
+    msg = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+    if (debug.on) return find('debug').html(msg.join('<br>'));
+  };
+
+  debug.on = true;
 
   hideMessage = function() {
     if (message.up) {
       $("#message").addClass('hidden');
+      find('messagehitbox').addClass('hidden');
       message.up = false;
       return $document.trigger('messageend');
     }
@@ -49,10 +72,26 @@
     if (message.up && e.which === 13 || e.which === 90) return hideMessage();
   });
 
-  $(function() {
+  $document.on('click', '#messagehitbox', function() {
+    return hideMessage();
+  });
+
+  $content.on('mousedown', function(e) {
+    var x, y;
+    if (control.person != null) {
+      x = e.pageX - this.offsetParent.offsetLeft;
+      y = e.pageY - this.offsetParent.offsetTop;
+      control.target = {
+        x: x,
+        y: y
+      };
+      return debug(x, y);
+    }
+  });
+
+  enterName = function() {
     var $nameInsert, enteredName;
-    $nameInsert = $('#nameinsert');
-    $nameInsert.show();
+    $nameInsert = show('nameinsert');
     $('#newname').focus();
     $nameInsert.find('input').on('keyup', function(e) {
       if (e.which === 13) return enteredName();
@@ -63,7 +102,7 @@
     return enteredName = function() {
       var newName;
       newName = $('#newname').val();
-      if (newName === "Googlohurf Harpstuck") {
+      if (newName === "Googlohurf Hoodchuck") {
         alert("Oh, come on! That's a horrible name.");
         $('#newname').focus();
         return;
@@ -71,19 +110,44 @@
       $nameInsert.find('.first').remove();
       return $nameInsert.find('.second').show().on('click', '.ok', function() {
         $nameInsert.remove();
-        show('scene1');
-        return setTimeout(scene1, 0);
+        return introYourRoom();
       });
     };
-  });
+  };
+
+  play1 = function() {
+    var $play;
+    $play = show('play1');
+    message("This is you! You're a pretty handsome dude :)");
+    return $document.on('messageend', function() {
+      return enterName();
+    });
+  };
+
+  introYourRoom = function() {
+    var $scene;
+    $scene = show('scene1');
+    message("This is your room. It's a bit dark right now, even if it means getting around is a little harder.");
+    return $document.one('messageend', function() {
+      $scene.find('.yourbroim').removeClass('hidden');
+      message("Looks like your bro is instant messaging you! You have an inkling of what he's got to say, and you don't want to miss it.");
+      return $document.one('messageend', function() {
+        return $scene.find('.yourbroim').on('click', function() {
+          $scene.find('.yourbroim').remove();
+          return scene1();
+        });
+      });
+    });
+  };
 
   scene1 = function() {
     var $scene;
-    $scene = find('scene1');
-    message("* LL began instant-messaging you!\nLL: Hello, Steve.\nLL: I just had another revelation.\nGQ: hahaha dude\nGQ: are you serious\nGQ: you have these like every other day\nGQ: every day*\nGQ: ok sorry what is it go on", "LL is typing...");
+    $scene = show('scene1');
+    $scene.find('.ggcomp').removeClass('hidden');
+    message("* LL began instant messaging you!\nLL: Hello, Steve.\nLL: I just had another revelation.\nGQ: hahaha dude\nGQ: are you serious\nGQ: you have these like every other day\nGQ: every day*\nGQ: ok sorry what is it go on", "LL is typing...");
     return $document.one('messageend', function() {
       $scene.find('.gghouse').removeClass('hidden');
-      message("LL: I think we're in a game.\nGQ: O_O\nLL: Let me talk.\nLL: So, I've been wondering.\nLL: Our lives really aren't that different from a video game character.\nLL: We level up, we have arbitrary stat attributes, and all that.\nLL: We even have NPCs.", "LL is typing...");
+      message("LL: I think we're in a game.\nGQ: O_O\nLL: No, let me talk.\nLL: So, I've been wondering.\nLL: Our lives really aren't that different from a video game character.\nLL: We level up, we have arbitrary stat attributes, and all that.\nLL: We even have NPCs.", "LL is typing...");
       return $document.one('messageend', function() {
         message("LL: And our world is called Ludum.\nLL: I don't know, it seems painfully obvious to me now.\nLL: So I did some more sleuthing.\nLL: By which I mean I entered some calculations.", "LL is typing...");
         return $document.one('messageend', function() {
@@ -94,7 +158,8 @@
             message("LL: And then I started wondering...\nLL: What lies beyond?\nLL: What lies sleeping?", "GQ is typing...");
             return $document.one('messageend', function() {
               $scene.find('.ggcomp').appendTo($scene);
-              return message("GQ: uh\nGQ: pretty melodramatic there bro!!\nLL: Okay, well, I guess it's not surprising that you'd act this way.\nLL: But, speaking as your friend, I know you're freaked out.\nLL: My work here is done.");
+              message("GQ: uh\nGQ: pretty melodramatic there bro!!\nLL: Okay, well, I guess it's not surprising that you'd act this way.\nLL: But, speaking as your younger brother by blood, you should really quit it.\nLL: And this isn't one of those times where the very next day I realize you were absolutely correct.\nLL: Anyway, I have to go fix my dimensional warper. It should be done very soon.\nLL: Be in my room in two minutes.\n* LL signed off.\nGQ: wait what\n* LL is no longer online!");
+              return $document.one('messageend', play2);
             });
           });
         });
@@ -102,10 +167,52 @@
     });
   };
 
-  scene2 = function() {};
+  play2 = function() {
+    var $scene;
+    $scene = show('scene1');
+    $scene.find('.ggcomp').remove();
+    $scene.find('.gghouse').remove();
+    $scene.find('.stars').remove();
+    $scene.find('.tentacles').remove();
+    message("Getting around this house is impossible! You know it'll take you way longer than two minutes. But you have to try. Your brother might be annoying slash amusing slash cute with his \"revelations,\" but he always makes cool stuff.");
+    return $document.one('messageend', function() {
+      message("Your mother is extremely private, so she installed switches everywhere to deter anyone from stealing her things (or her kids). She is especially protective of your little brother and never lets him unlock his own door.\n\nYou can't remember the exact sequences to the switches, because she changes them every night. Your mother somehow manages to change them without you noticing. And sometimes, some switches depend on another switch being pressed.\n\nYou have no idea how she keeps track of all these switches in her head. It would be impressive if it wasn't SO INFURIATING.");
+      return $document.one('messageend', function() {
+        $scene.find('.doorswitch').removeClass('hidden');
+        $scene.find('.lightswitch').removeClass('hidden');
+        $scene.find('.door').removeClass('hidden');
+        $scene.find('.door').on('click', function() {
+          if (state.yourDoorLocked) {
+            return message("The door is locked!");
+          } else {
+            return scene2();
+          }
+        });
+        $scene.find('.doorswitch').on('click', function() {
+          if (state.flippedLight) {
+            message("You hear a *click*.");
+            return state.yourDoorLocked = false;
+          } else {
+            return message("The switch won't budge! It must depend on another switch...");
+          }
+        });
+        return $scene.find('.lightswitch').on('click', function() {
+          if (state.flippedLight) {
+            message("You turn off the lights.\n\nYou also hear a *click*.");
+            return state.flippedLight = false;
+          } else {
+            message("You turn on the lights. You also resist a light pun.\n\nYou also hear a *click*.");
+            return state.flippedLight = true;
+          }
+        });
+      });
+    });
+  };
 
-  $(function() {
-    return scene1();
-  });
+  scene2 = function() {
+    return message("Navigate!");
+  };
+
+  $(play2);
 
 }).call(this);
